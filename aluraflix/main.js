1e90dd1361f70d3120d49c9cd3928092d0b39a24
tiago graffunder <tiagopinzgraffunder@gmail.com>
2023-02-01 14:35:54 -0300

    day 03

---
+++ aluraflix/main.js
@@ -0,0 +1,5 @@
+var listaFilmes =["http://www.secbotucatu.org.br/site/wp-content/uploads/2017/06/260x365_519ebbddb7fe8.jpg","https://www.omelete.com.br/imagens/cinema/news/looney_tunes/1.jpg","https://br.web.img3.acsta.net/medias/nmedia/18/95/49/93/20409082.jpg","https://upload.wikimedia.org/wikipedia/pt/thumb/a/a4/Space_Jam_A_New_Legacy.jpg/250px-Space_Jam_A_New_Legacy.jpg"];
+
+for(i=0; i < listaFilmes.length; i++){
+  document.write("<img src="+listaFilmes[i]+">");
+}
