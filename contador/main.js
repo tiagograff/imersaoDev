6cfcbeb6053f4651c9166b53241c79ba8f363cb9
tiago graffunder <tiagopinzgraffunder@gmail.com>
2023-02-01 13:20:16 -0300

    day 01

---
+++ contador/main.js
@@ -0,0 +1,7 @@
+var nome = 'tiago graffunder';
+
+var anoLuz = 4.2465;
+var anoMetros = 9.461e+15;
+
+anoConvertido = (anoLuz*anoMetros);
+alert(anoLuz+' ano(s) luz é equivalente a '+anoConvertido+' metros');
